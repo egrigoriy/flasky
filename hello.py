f9a3b4b2da7d433731b1218ab90a0acf64b37bae
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2019-12-11 14:49:43 +0000

    Chapter 4: Redirect and User Sessions (4b)

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -18,11 +18,10 @@ class NameForm(FlaskForm):
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = NameForm()
-    name = None
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ""
-    return render_template("index.html", form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, name=session.get('name'))
 
 @app.route("/user/<name>")
 def user(name):
